618705fab4b5abda5e91516acc48ca017310501a
rocketeerli <rocketeerli@qq.com>
2021-03-08 22:44:18 +0800

    feat: weixin pay

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -5,52 +5,70 @@ Page({
    * 页面的初始数据
    */
   data: {
+    // carts: [
+    //   {
+    //     img: "/images/goods/maidong.jpg",
+    //     name: "脉动",
+    //     price: 3,
+    //     number: 9
+    //   },{
+    //     img: "/images/goods/KSFH.jpg",
+    //     name: "康师傅红茶",
+    //     price: 3,
+    //     number: 3
+    //   }, {
+    //     img: "/images/goods/fenda.jpg",
+    //     name: "芬达",
+    //     price: 3,
+    //     number: 9
+    //   },{
+    //     img: "/images/goods/jiaduobao.jpg",
+    //     name: "加多宝",
+    //     price: 3,
+    //     number: 8
+    //   }, {
+    //     img: "/images/goods/LLDS.jpg",
+    //     name: "维他命",
+    //     price: 3,
+    //     number: 3
+    //   }, {
+    //     img: "/images/goods/MZY.jpg",
+    //     name: "美汁源",
+    //     price: 3,
+    //     number: 9
+    //   },{
+    //     img: "/images/goods/TYCL.jpg",
+    //     name: "统一CL",
+    //     price: 3,
+    //     number: 8
+    //   }, {
+    //     img: "/images/goods/TYYC.jpg",
+    //     name: "统一YC",
+    //     price: 3,
+    //     number: 3
+    //   }, {
+    //     img: "/images/goods/yingyangkuaixian.jpg",
+    //     name: "营养快线",
+    //     price: 3,
+    //     number: 3
+    //   }
+    // ],
     carts: [
       {
-        img: "/images/goods/maidong.jpg",
-        name: "脉动",
+        name: "得力9585彩色加厚型垃圾袋",
         price: 3,
-        number: 9
-      },{
-        img: "/images/goods/KSFH.jpg",
-        name: "康师傅红茶",
-        price: 3,
-        number: 3
+        number: 1,
+        img: "/images/test-goods/garbage-bag.jpg"
       }, {
-        img: "/images/goods/fenda.jpg",
-        name: "芬达",
-        price: 3,
-        number: 9
-      },{
-        img: "/images/goods/jiaduobao.jpg",
-        name: "加多宝",
-        price: 3,
-        number: 8
+        name: "黑色折叠式雨伞",
+        price: 19,
+        number: 2,
+        img: "/images/test-goods/umbrella.jpg"
       }, {
-        img: "/images/goods/LLDS.jpg",
-        name: "维他命",
-        price: 3,
-        number: 3
-      }, {
-        img: "/images/goods/MZY.jpg",
-        name: "美汁源",
-        price: 3,
-        number: 9
-      },{
-        img: "/images/goods/TYCL.jpg",
-        name: "统一CL",
-        price: 3,
-        number: 8
-      }, {
-        img: "/images/goods/TYYC.jpg",
-        name: "统一YC",
-        price: 3,
-        number: 3
-      }, {
-        img: "/images/goods/yingyangkuaixian.jpg",
-        name: "营养快线",
-        price: 3,
-        number: 3
+        name: "番茄牌TOMATO事务用剪刀",
+        price: 5,
+        number: 1,
+        img: "/images/test-goods/scissors.jpg"
       }
     ],
     totalMoney: 0
--- pages/goods/goods.js
+++ pages/goods/goods.js
@@ -5,67 +5,88 @@ Page({
    * 页面的初始数据
    */
   data: {
+    // goods: [
+    //   {
+    //     id: "001",
+    //     name: "百岁山",
+    //     price: 3,
+    //     number: 6,
+    //     img: "/images/goods/BSS.jpg"
+    //   }, {
+    //     id: "002",
+    //     name: "康师傅红茶",
+    //     price: 3,
+    //     number: 6,
+    //     img: "/images/goods/KSFH.jpg"
+    //   }, {
+    //     id: "003",
+    //     name: "维他命",
+    //     price: 3,
+    //     number: 6,
+    //     img: "/images/goods/LLDS.jpg"
+    //   }, {
+    //     id: "004",
+    //     name: "美汁源",
+    //     price: 3,
+    //     number: 6,
+    //     img: "/images/goods/MZY.jpg"
+    //   }, {
+    //     id: "005",
+    //     name: "统一CL",
+    //     price: 3,
+    //     number: 6,
+    //     img: "/images/goods/TYCL.jpg"
+    //   }, {
+    //     id: "006",
+    //     name: "统一YC",
+    //     price: 3,
+    //     number: 6,
+    //     img: "/images/goods/TYYC.jpg"
+    //   }, {
+    //     id: "007",
+    //     name: "芬达",
+    //     price: 3,
+    //     number: 6,
+    //     img: "/images/goods/fenda.jpg"
+    //   }, {
+    //     id: "008",
+    //     name: "加多宝",
+    //     price: 3,
+    //     number: 6,
+    //     img: "/images/goods/jiaduobao.jpg"
+    //   }, {
+    //     id: "009",
+    //     name: "脉动",
+    //     price: 3,
+    //     number: 6,
+    //     img: "/images/goods/maidong.jpg"
+    //   }, {
+    //     id: "010",
+    //     name: "营养快线",
+    //     price: 3,
+    //     number: 6,
+    //     img: "/images/goods/yingyangkuaixian.jpg"
+    //   }
+    // ]
     goods: [
       {
-        id: "001",
-        name: "百岁山",
+        id: "011",
+        name: "得力9585彩色加厚型垃圾袋",
         price: 3,
         number: 6,
-        img: "/images/goods/BSS.jpg"
+        img: "/images/test-goods/garbage-bag.jpg"
       }, {
-        id: "002",
-        name: "康师傅红茶",
-        price: 3,
-        number: 6,
-        img: "/images/goods/KSFH.jpg"
+        id: "012",
+        name: "黑色折叠式雨伞",
+        price: 19,
+        number: 7,
+        img: "/images/test-goods/umbrella.jpg"
       }, {
-        id: "003",
-        name: "维他命",
-        price: 3,
-        number: 6,
-        img: "/images/goods/LLDS.jpg"
-      }, {
-        id: "004",
-        name: "美汁源",
-        price: 3,
-        number: 6,
-        img: "/images/goods/MZY.jpg"
-      }, {
-        id: "005",
-        name: "统一CL",
-        price: 3,
-        number: 6,
-        img: "/images/goods/TYCL.jpg"
-      }, {
-        id: "006",
-        name: "统一YC",
-        price: 3,
-        number: 6,
-        img: "/images/goods/TYYC.jpg"
-      }, {
-        id: "007",
-        name: "芬达",
-        price: 3,
-        number: 6,
-        img: "/images/goods/fenda.jpg"
-      }, {
-        id: "008",
-        name: "加多宝",
-        price: 3,
-        number: 6,
-        img: "/images/goods/jiaduobao.jpg"
-      }, {
-        id: "009",
-        name: "脉动",
-        price: 3,
-        number: 6,
-        img: "/images/goods/maidong.jpg"
-      }, {
-        id: "010",
-        name: "营养快线",
-        price: 3,
-        number: 6,
-        img: "/images/goods/yingyangkuaixian.jpg"
+        id: "013",
+        name: "番茄牌TOMATO事务用剪刀",
+        price: 5,
+        number: 10,
+        img: "/images/test-goods/scissors.jpg"
       }
     ]
   },
